f361df5b60c65aab4f3188105f05cb8b01b5cc9a
ShishuTochi <PixelTochi@gmail.com>
2024-07-27 10:39:10 +0300

    More fixes...

--- src/components/NotFoundSection.jsx
+++ src/components/NotFoundSection.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import HatMan from "../assets/hat_man.webp"
 
 export default function NotFoundSection() {
     return (
         <section id='not-found-section' className='text-center' style={{margin:"128px 0px"}}>
             <h1 className='position-relative'>
-                <img className='position-absolute' style={{left:"-130px", top: "-70px"}} src={HatMan} alt="hat_man.webp" />
+                <img className='position-absolute' style={{left:"-130px", top: "-70px"}} src='hat_man.webp' alt="hat_man.webp" />
                 404
             </h1>
             <p>Page Not Found. Return <Link to='/'>Home</Link>?</p>
