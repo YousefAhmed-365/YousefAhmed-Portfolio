25d076f2587edcbf3462ffd04987295863a546c3
ShishuTochi <PixelTochi@gmail.com>
2024-07-27 09:34:51 +0300

    Switched to bootstrapCDN for better optimizations

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
-import "./Index.scss"
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Collapse } from 'react-bootstrap'
+import "./Index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
